03d48e3150e26add9138ccb1b0c81fc72a6d5f27
naimur <naimur202163@gmail.com>
2023-01-10 14:08:24 +0600

    login logo added

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 export default function Login() {
+  const imageURL = ` https://i.ibb.co/3hNXKq6/logo-blog-sesta-trasparente.png`;
   return (
-    <div>
-        Hello From Login
-    </div>
-  )
+    <Box>
+      <img src={imageURL} alt="login" />
+    </Box>
+  );
 }
