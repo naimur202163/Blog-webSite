6c346ef0749879b800fceec826e7831e899cbce2
naimur <naimur202163@gmail.com>
2023-01-10 18:51:37 +0600

    bug fixed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+const PORT=8000
 //components
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-
+import connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
@@ -15,13 +14,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
+app.use("/", Router);
+connection();
 
-
-const PORT = 8000;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-
-Connection(username, password);
-
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`The server is running on localhost:https:${PORT}`);
+});
+// Db
